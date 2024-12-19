5cff612d524f888e84cb5833638edb6cca53dd79
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-12-19 14:31:15 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('inspira_home', 'postgres', 'tazamaza03', {
-  host: 'localhost',
+const sequelize = new Sequelize('postgresql://inspira_home_bg4o_user:8rYimYi35fhBbGKtfDwuBsm82Drx5jv0@dpg-cthg6f5ds78s73f96bm0-a.oregon-postgres.render.com/inspira_home_bg4o', {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
